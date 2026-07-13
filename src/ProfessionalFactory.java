8b794a17c05891802c8ed46af1551bb98045c55d
Zeshan Asif <asifz22@farmingdale.edu>
2026-07-12 22:25:49 -0400

    Create ProfessionalFactory class.java

---
+++ src/ProfessionalFactory.java
@@ -0,0 +1,17 @@
+public class ProfessionalFactory implements GarmentFactory {
+
+    @Override
+    public Top createTop() {
+        return new ProfessionalTop();
+    }
+
+    @Override
+    public Pants createPants() {
+        return new ProfessionalPants();
+    }
+
+    @Override
+    public Shoes createShoes() {
+        return new ProfessionalShoes();
+    }
+}
