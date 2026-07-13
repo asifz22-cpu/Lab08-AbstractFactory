aa47f83b32600734bebc3326c54ee108d1dfaa85
Zeshan Asif <asifz22@farmingdale.edu>
2026-07-12 22:28:48 -0400

    Create CasualFactory class.java

---
+++ src/CasualFactory.java
@@ -0,0 +1,17 @@
+public class CasualFactory implements GarmentFactory {
+
+    @Override
+    public Top createTop() {
+        return new CasualTop();
+    }
+
+    @Override
+    public Pants createPants() {
+        return new CasualPants();
+    }
+
+    @Override
+    public Shoes createShoes() {
+        return new CasualShoes();
+    }
+}
