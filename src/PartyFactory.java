8fdbe24916ddb8233e4cffbcf3772b9e122c0447
Zeshan Asif <asifz22@farmingdale.edu>
2026-07-12 22:32:50 -0400

    Create PartyFactory class.java

---
+++ src/PartyFactory.java
@@ -0,0 +1,17 @@
+public class PartyFactory implements GarmentFactory {
+
+    @Override
+    public Top createTop() {
+        return new PartyTop();
+    }
+
+    @Override
+    public Pants createPants() {
+        return new PartyPants();
+    }
+
+    @Override
+    public Shoes createShoes() {
+        return new PartyShoes();
+    }
+}
